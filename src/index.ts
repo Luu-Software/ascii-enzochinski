21a2bcf38db0cf95941d715f2f8d9ee99f182a4e
enzochinski <e.chinski@wolfsohn.edu.ar>
2026-06-17 15:50:27 -0300

    h jgvb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
+let resultado: string = ''; 
+let contador : number = 0;
+while ( contador < cantidad ) {
+  console.log (contador)
+  contador = contador + 1;
+}
+return  resultado;
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
-// COMPLETAR
+let fila:number=0;
+while (fila<tamaño){
+  console.log(repetirLetra(caracter, tamaño))
+  fila=fila + 1
+}
+
